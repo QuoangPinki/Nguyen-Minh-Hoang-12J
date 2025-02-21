4e09092d8bbfd9be0e0d7e7b00651a6331888407
QuoangPinki <nguyenhoangpro4@gmail.com>
2025-02-21 15:33:33 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-
+function login()
+{
+  let u = username.value
+  let p = pw.value
+  if (u=="Hoang" && p=="123")
+  {
+    window.open("home.html")
+    document.getElementById(compiler).style.display="block"
+  }
+}
